571cfe053d014c72805ce9c06a3b53acb75246ed
Mehmet <mehmet.h.yurdakul@gmail.com>
2022-10-15 12:16:31 +0200

    animations and dark-mode fix

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 import { inView, animate, scroll } from "https://cdn.skypack.dev/motion";
 
-document.querySelectorAll("section > article").forEach((item) => {
-  scroll(animate(item, { opacity: [0, 1, 1, 0] }), {
-    target: item,
-    offset: ["start end", "end end", "start start", "end start"],
-  });
+// document.querySelectorAll("section > article").forEach((item) => {
+//   scroll(animate(item, { opacity: [0, 1, 1, 0] }), {
+//     target: item,
+//     offset: ["start end", "end end", "start start", "end start"],
+//   });
+// });
+
+inView("section", ({ target }) => {
+  animate(
+    target.querySelector(".fade"),
+    { opacity: 1, x: [-70, -50, 0] },
+    { delay: 0.2, duration: 0.9, easing: [0.17, 0.55, 0.55, 1] }
+  );
 });
 
 const toggle = document.querySelector(".toggle");
